f64a43bde2a35fc9211e7d1dc0a0aff6c7e5a945
dev0lt <igorinho07@o2.pl>
2023-12-08 12:43:19 +0100

    fixed game logic (still needs refining) and added CSS

--- script.js
+++ script.js
@@ -4,29 +4,68 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-const playerScore = document.getElementById("score_player");
-const cpuScore = document.getElementById("score_cpu");
+const playerScoreHTML = document.getElementById("score_player");
+const cpuScoreHTML = document.getElementById("score_cpu");
+
+const reset = document.getElementById("reset");
+const log = document.getElementById("log");
 
 let arr = ["rock", "paper", "scissors"];
 
-////////// PC CHOICE //////////////////
+let playerScore = 0;
+let computerScore = 0;
 
 let computerChoice;
 
-/////////// PLAYER CHOICE ////////////////////
-
-let playerChoice = "";
-
 ////////// GAME //////////////////////////
 
-let getComputerChoice = function () {
-  computerChoice = arr.at(Math.floor(Math.random() * 3));
-  if (playerChoice === "rock" && computerChoice !== playerChoice) {
-    alert("u won");
-  } else alert("u lost");
-};
-getComputerChoice();
+rock.addEventListener("click", () => game("rock"));
+paper.addEventListener("click", () => game("paper"));
+scissors.addEventListener("click", () => game("scissors"));
 
-rock.addEventListener("click", () => (playerChoice = "rock"));
-paper.addEventListener("click", () => (playerChoice = "paper"));
-scissors.addEventListener("click", () => (playerChoice = "scissors"));
+function getComputerChoice() {
+  computerChoice = arr.at(Math.floor(Math.random() * 3));
+  return computerChoice;
+}
+
+function game(player) {
+  let computer = getComputerChoice();
+
+  /////// player wins the round //////
+  if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer == "paper")
+  ) {
+    playerScore++;
+    playerScoreHTML.textContent = playerScore;
+    log.textContent = `Player: ${player} CPU: ${computer} = you have won this round!`;
+
+    getComputerChoice();
+
+    /////// computer wins the round //////
+  } else if (
+    (player === "rock" && computer === "paper") ||
+    (player === "paper" && computer === "scissors") ||
+    (player === "scissors" && computer == "rock")
+  ) {
+    computerScore++;
+    cpuScoreHTML.textContent = computerScore;
+    log.textContent = `Player: ${player}, CPU: ${computer} = cpu has won this round!`;
+
+    getComputerChoice();
+
+    ////////////// draw /////////////////
+  } else {
+    log.textContent = `Player: ${player}, CPU: ${computer} = it's a draw!`;
+
+    getComputerChoice();
+    return;
+  }
+}
+
+reset.addEventListener("click", function () {
+  playerScoreHTML.textContent = 0;
+  cpuScoreHTML.textContent = 0;
+  log.textContent = "";
+});
